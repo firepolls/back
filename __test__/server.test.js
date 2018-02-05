384dd13a553abb320a3cc9673f1bd3e3acba8eae
Robert Reed <robert.mc.reed@gmail.com>
2018-02-05 11:52:05 -0800

    combined server/db into a single describe block

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -5,29 +5,23 @@ require('./lib/setup');
 import * as db from '../src/lib/db';
 import * as server from '../src/lib/server';
 
-describe('server and db setup', () => {
-  describe('db.js', () => {
-    test('db should return an error if you try to disconnect when not connected', () => {
-      return db.disconnect()
-        .then(Promise.reject)
-        .catch(error => {
-          expect(error.message).toEqual('__ERROR__ Not connected to DB');
-        });
-    });
+describe('server and db fail safes', () => {
+  test('server should return an error if stopped while already off.', () => {
+    return server.stop()
+      .then(Promise.reject)
+      .catch(error => {
+        expect(error.message).toEqual('__ERROR__ Server is already off');
+      });
   });
-  
-  describe('server.js', () => {
-    test('server should return an error if stopped while already off.', () => {
-      return server.stop()
-        .then(Promise.reject)
-        .catch(error => {
-          expect(error.message).toEqual('__ERROR__ Server is already off');
-        });
-    });
-  });  
-});
 
-describe('throw away', () => {
+  test('db should return an error if you try to disconnect when not connected', () => {
+    return db.disconnect()
+      .then(Promise.reject)
+      .catch(error => {
+        expect(error.message).toEqual('__ERROR__ Not connected to DB');
+      });
+  });
+
   test('db should return an error if you try to connect while already connected', () => {
     return db.connect(true)
       .then(Promise.reject)
